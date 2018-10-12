29a60cae1ffccdc3b85a1fafd443bd334553978a
Ivan Beauté <ivan@fabrique-it.fr>
2018-10-12 11:50:18 +0200

    Fix threading.timer multiple args calls

--- DevOps/Projet2/api-store-to-db/__main__.py
+++ DevOps/Projet2/api-store-to-db/__main__.py
@@ -12,7 +12,7 @@ def get_bracelet_id(api_bracelet):
 
 
 def get_bracelet_footstep(api_bracelet, collection):
-    threading.Timer(3600, get_bracelet_footstep, [collection]).start()
+    threading.Timer(3600, get_bracelet_footstep, [api_bracelet, collection]).start()
 
     bracelet_id = get_bracelet_id(api_bracelet)
     response = requests.get('http://{}/footstep/'.format(api_bracelet) + bracelet_id)
@@ -22,7 +22,7 @@ def get_bracelet_footstep(api_bracelet, collection):
 
 
 def get_bracelet_heartbeat(api_bracelet, collection):
-    threading.Timer(10, get_bracelet_heartbeat, [collection]).start()
+    threading.Timer(10, get_bracelet_heartbeat, [api_bracelet, collection]).start()
 
     bracelet_id = get_bracelet_id(api_bracelet)
     response = requests.get('http://{}/heartbeat/'.format(api_bracelet) + bracelet_id)
